97f18ddd2ca934a41e273b866f8c947cac9db907
zeeshan mustafa <zeeshan.mustafa@yahoo.com>
2018-02-05 13:15:13 -0500

    hope i did the submission part right

--- OOP/01_31/BeerSongClArg/BeerSong.java
+++ OOP/01_31/BeerSongClArg/BeerSong.java
@@ -34,7 +34,7 @@ public class BeerSong {
             System.out.println(beerNum + " " + word + " of beer on the wall");
             System.out.println(beerNum + " " + word + " of beer.");
             System.out.println("Take one down.");
-            System.out.println("Pass it around.") ;
+            System.out.println("Pass "+takeDownNum " around.") ;
             beerNum -= takeDownNum;
             
             if (beerNum > 0) {
